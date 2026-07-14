b9618f178cf67afbd942e752c86b4b4071f3a904
Diego <diego.trb25@gmail.com>
2026-07-13 21:56:43 -0300

    v1.0.2

--- src/lib/sequences.ts
+++ src/lib/sequences.ts
@@ -1,18 +1,18 @@
 export const sequences = {
   hero: {
-    baseUrl: 'https://eilprjrwppfomtqsrlde.supabase.co/storage/v1/object/public/Webp%20Sequence%201',
+    baseUrl: 'https://dnarxazvdcvykgexcllu.supabase.co/storage/v1/object/public/Atmos',
     frameCount: 192,
   },
   interior: {
-    baseUrl: 'https://eilprjrwppfomtqsrlde.supabase.co/storage/v1/object/public/Webp%20Sequence%202',
+    baseUrl: 'https://dnarxazvdcvykgexcllu.supabase.co/storage/v1/object/public/Atmos2',
     frameCount: 192,
   },
   nature: {
-    baseUrl: 'https://eilprjrwppfomtqsrlde.supabase.co/storage/v1/object/public/Webp%20Sequence%203',
+    baseUrl: 'https://dnarxazvdcvykgexcllu.supabase.co/storage/v1/object/public/Atmos3',
     frameCount: 192,
   },
   booking: {
-    baseUrl: 'https://eilprjrwppfomtqsrlde.supabase.co/storage/v1/object/public/Webp%20Sequence%204',
+    baseUrl: 'https://dnarxazvdcvykgexcllu.supabase.co/storage/v1/object/public/Atmos4',
     frameCount: 192,
   },
 };
